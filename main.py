ed19952790fcf544ec8a67ec8054993ac642d73f
Laís Dib Albuquerque <lda.snf20@uea.edu.br>
2021-06-08 23:17:54 -0400

    adding conection teacher and subject

--- main.py
+++ main.py
@@ -5,26 +5,30 @@ import teacher
 
 
 def main():
-     teachers_names = ["Leandro", "Fábio", "Levi", "Gustavo", "Laís"]
-     teachers_list = []
+    teachers_names = ["Jucimar", "Elloá", "Edgar", "Sílvia Cristina", "Márcia"]
+    teachers_list = []
 
-     for i in range(5):
-          teachers_list.append(teacher.Teacher(teachers_names[i], i + 1))
+    for i in range(5):
+        teachers_list.append(teacher.Teacher(teachers_names[i], i + 1))
 
-     subjects_names = ["MD", "Cálculo I", "Álgebra Linear", "PCA", "OAC"]
-     subjects_list = []
-     
-     for i in range(5):
-          # Formatting the code to ESTCMP001, ESTCMP002, ESTCMP003, ...
-          subjects_list.append(subject.Subject(subjects_names[i], "ESTCMP%03d" % (i + 1)))
+    subjects_names = ["MD", "Cálculo I", "OAC", "PCA", "LPC I"]
+    subjects_list = []
 
-     print("TEACHERS:")
-     for i in range(5):
-          teachers_list[i].output_teacher()
+    for i in range(5):
+        # Formatting the code to SB001, SB002, SB003, ...
+        subjects_list.append(subject.Subject(subjects_names[i], "SB%03d" % (i + 1)))
 
-     print("\nSUBJECTS:")
-     for i in range(5):
-          subjects_list[i].output_subject()
+    for i in range(5):
+        teachers_list[i].add_subject(subjects_list[i])
+        subjects_list[i].add_teacher(teachers_list[i])
+
+    print("TEACHERS:")
+    for i in range(5):
+        teachers_list[i].output_teacher()
+
+    print("\nSUBJECTS:")
+    for i in range(5):
+        subjects_list[i].output_subject()
 
 
 main()
--- teacher.py
+++ teacher.py
@@ -1,12 +1,18 @@
-
 class Teacher:
 
     teacher_name = ""
-    teacher_code = 0 # Número de 3 dígitos
+    teacher_code = 0  # Número de 3 dígitos
+    subjects_list = []
 
     def __init__(self, name, code):
         self.teacher_name = name
         self.teacher_code = code
 
     def output_teacher(self):
-        print(self.teacher_name, "%03d" % self.teacher_code)
+        print(self.teacher_name, "%03d" % self.teacher_code, self.subjects_list)
+
+    def add_subject(self, subject):
+        self.subjects_list.append(subject)
+
+    def __str__(self):
+        return "%s %d" % (self.teacher_name, self.teacher_code)
