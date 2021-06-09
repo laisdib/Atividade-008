d10da935ab6ad365f53ec7d5bff56b82731e275d
ghffadel <ghff.snf20@uea.edu.br>
2021-06-08 22:45:06 -0400

    Adding teachers and subjects data

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 # Exercise 008: Class Diagram
-import universidade
+
+import subject
+import teacher
 
 
 def main():
-     print()
+     teachers_names = ["Leandro", "Fábio", "Levi", "Gustavo", "Laís"]
+     teachers_list = []
+
+     for i in range(5):
+          teachers_list.append(teacher.Teacher(teachers_names[i], i + 1))
+
+     subjects_names = ["MD", "Cálculo I", "Álgebra Linear", "PCA", "OAC"]
+     subjects_list = []
+     
+     for i in range(5):
+          # Formatting the code to ESTCMP001, ESTCMP002, ESTCMP003, ...
+          subjects_list.append(subject.Subject(subjects_names[i], "ESTCMP%03d" % (i + 1)))
+
+     print("TEACHERS:")
+     for i in range(5):
+          teachers_list[i].output_teacher()
+
+     print("\nSUBJECTS:")
+     for i in range(5):
+          subjects_list[i].output_subject()
 
 
 main()
